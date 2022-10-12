035e679ee12bde6b9c5fb8f7dbd75ce014f98ae1
Diba <dibaaamir80@gmail.com>
2022-10-12 17:00:47 +0330

    [ADD] add and cancel button

--- src/components/ExpensesList/ExpensesList.js
+++ src/components/ExpensesList/ExpensesList.js
@@ -2,8 +2,6 @@ import './ExpensesList.css';
 import ExpenseItem from '../ExpenseItem/ExpenseItem';
 
 const ExpensesList = (props) => {
-  let expensesContent = <p>No Expenses Found!</p>;
-
   if (props.items.length === 0)
     return <h2 className="expenses-list__fallback">Found no expenses</h2>;
   return (
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,35 @@
 import './NewExpense.css';
 import ExpenseForm from './components/ExpenseForm/ExpenseForm';
+import { useState } from 'react';
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+
   const onSaveNewExpenseHandler = (expense) => {
     const expenses = {
       id: Math.random().toString(),
       ...expense,
     };
     props.addNewExpence(expenses);
+    setShowForm(false);
+  };
+  const startEditingHandler = () => {
+    setShowForm(true);
+  };
+  const stopEditingHandler = () => {
+    setShowForm(false);
   };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveNewExpense={onSaveNewExpenseHandler} />
+      {!showForm && (
+        <button onClick={startEditingHandler}>Add new expense</button>
+      )}
+      {showForm && (
+        <ExpenseForm
+          onSaveNewExpense={onSaveNewExpenseHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
